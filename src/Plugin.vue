8df0cf66ff2680471b7f2e8abb8d58f59ba21fb2
DoomyCrystal <shagix.ix@gmail.com>
2023-03-08 14:52:48 +0100

    Feat: Create select with components as options

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,39 +1,39 @@
 <template>
   <div>
-    Google snippet preview:
-    <div class="p-metatags__preview">
-      <div class="p-metatags__google-title">{{ model.title || 'Your title' }}</div>
-      <div class="p-metatags__google-link">yoursite.com/example</div>
-      <div class="p-metatags__google-description">{{ model.description || 'Your description' }}</div>
-    </div>
-    <div class="uk-form-row">
-      <label>Meta Title</label>
-      <input type="text" placeholder="Your title" v-model="model.title" class="uk-width-1-1">
-    </div>
-
-    <div class="uk-form-row">
-      <label>Meta description</label>
-      <textarea rows="4" placeholder="Your description" v-model="model.description" class="uk-width-1-1"></textarea>
+    <div>
+      <select v-model="model.blok" class="select">
+        <option value="">Bitte Block wählen ...</option>
+        <option v-for="component in components" :value="component" v-bind:key="component._uid">
+          {{ component.name }}
+        </option>
+      </select>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      components: []
+    }
+  },
   mixins: [window.Storyblok.plugin],
   methods: {
     initWith() {
       return {
-        // needs to be equal to your storyblok plugin name
-        plugin: 'my-plugin-name',
-        title: '',
-        description: ''
+        plugin: 'sai-blok-selector',
+        blok: ''
       }
     },
-    pluginCreated() {
-      // eslint-disable-next-line
-      console.log('View source and customize: https://github.com/storyblok/storyblok-fieldtype')
+    async fetchComponents() {
+      const res = await fetch(`https://api.storyblok.com/v2/cdn/stories/${this.options.slug}?token=${this.options.token}`);
+      const data = await res.json();
+      this.components = data.story ? data.story.content[this.options.wrapper] : [];
     }
+  },
+  mounted() {
+    this.fetchComponents()
   },
   watch: {
     'model': {
@@ -47,19 +47,8 @@ export default {
 </script>
 
 <style>
-  .p-metatags__google-title {
-    color: blue;
-    text-decoration: underline;
-  }
-
-  .p-metatags__google-link {
-    color: green;
-  }
-
-  .p-metatags__preview {
-    margin: 5px 0 15px;
-    padding: 10px;
-    color: #000;
-    background: #FFF;
+  .select {
+    width: 100%;
+    border-radius: 4px;
   }
 </style>
